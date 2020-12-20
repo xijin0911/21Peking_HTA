82dd19aee198defeebe3c727c2f942b031d7d930
xijin0911 <chenxijin2017@gmail.com>
2020-12-20 15:41:12 +0000

    1220updates

--- function/transform_func.R
+++ function/transform_func.R
@@ -21,3 +21,19 @@ OddsToProb <- function(odds){
 ProbFactor <- function(prob,factor){
   OddsToProb(factor*ProbToOdds(prob))  
 }
+
+plot_trace <- function(m_M) {
+  df_M      <- data.frame(Cycle = 0:n_t, m_M, check.names = F)
+  df_M_long <- tidyr::gather(df_M, key = `Health State`, value, 2:ncol(df_M))
+  df_M_long$`Health State` <- factor(df_M_long$`Health State`, levels = v_names_states)
+  p <- ggplot2::ggplot(df_M_long, aes(x = Cycle, y = value, 
+                                      color = `Health State`, linetype = `Health State`)) +
+    geom_line(size = 1) +
+    xlab("Cycle") +
+    ylab("Proportion of the cohort") +
+    theme_bw(base_size = 14) +
+    theme(legend.position  = "bottom", 
+          legend.background = element_rect(fill = NA)) 
+  
+  return(p) 
+}
