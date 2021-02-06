1ef2b0665bc44a935096be515fa7ba5f4f7f1b9a
xijin0911 <chenxijin2017@gmail.com>
2021-02-06 18:31:14 +0000

    FangShan Update

--- function/transform_func.R
+++ function/transform_func.R
@@ -3,18 +3,14 @@ RateToProb <- function(rate, t){ # t is the cycle length
   prob <- 1-exp(-rate*t)
   prob
 }
-
-
 ProbToOdds <- function(prob){
   odds <- prob/(1-prob)
   odds
 }
-
 OddsToProb <- function(odds){
   prob <- odds/(1+odds)
   prob
 }
-
 # First converts the probability to odds
 # then multiplies it by the given factor
 # then converts it back to a probability
